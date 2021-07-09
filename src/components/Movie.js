ac3e1b6654f631f634a0f69355bd6f2f7a7b112b
Yovana Soto <yovana.sm@gmail.com>
2021-07-09 15:44:45 -0400

    added movie presentational component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,29 @@
 import React from "react";
 
-const Movie = () => {
+const Movie = ({ movie }) => {
+  const style = {
+    height: "260px",
+    objectFit: "cover",
+  };
+  const isFavorite = true;
   return (
-    <div>
-      <h1>Movie</h1>
+    <div className="card">
+      <img
+        src={movie.imageUrl}
+        alt={movie.title}
+        className="card-img-top"
+        style={style}
+      />
+      <div className="card-body">
+        <h1>Movie</h1>
+        <button
+          className={`btn ${
+            isFavorite ? "btn-success" : "btn-outline-primary"
+          }`}
+        >
+          Favorite
+        </button>
+      </div>
     </div>
   );
 };
