101f1ab0dc7138cff94540862ddc574592ec318e
Yovana Soto <yovana.sm@gmail.com>
2021-07-09 14:58:42 -0400

    Add presentation component navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 const Navbar = () => {
+  const user = { name: "Yovi" };
   return (
-    <div>
-      <h1>Nabar</h1>
-    </div>
+    <nav className="navbar navbar-dark bg-dark mb4">
+      <div className="container">
+        <span className="navbar-brand">
+          <h2>{user ? `Hello ${user.name}` : "Welcome@"}</h2>
+        </span>
+        {user ? (
+          <button className="btn btn-primary"> Sign out</button>
+        ) : (
+          <button className="btn btn-primary"> Sign in</button>
+        )}
+      </div>
+    </nav>
   );
 };
 
